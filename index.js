6931e2da97cebd04d0212b64a18e63501d6c34c8
loveinsyrup178-glitch <loveinsyrup178@gmail.com>
2025-12-13 07:40:38 -0600

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,6 @@ return new EmbedBuilder()
 .setDescription(
 '**Welcome, sipper.**\n' +
 'Tap the lean cup below to verify & unlock the rest of the server.\n\n' +
-'<:whitelean:1376495549179756607> ・ <a:purplewaves:1234567890123456789>'
 )
 .setImage(VERIFY_IMG)
 .setFooter({ text: 'Verification required • WOCKHARDT' });
